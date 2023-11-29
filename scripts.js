6568755d357d5a1e4889213433b0a595f43ef4f2
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-30 00:22:05 +0600

    add info toast

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const toastBox = document.querySelector(".js-toast-box");
 const succefullBtn = document.querySelector(".js-successful-btn");
 const errorBtn = document.querySelector(".js-error-btn");
 const invalidBtn = document.querySelector(".js-invalid-btn");
+const infoBtn = document.querySelector(".js-info-btn");
 
 succefullBtn.addEventListener("click", () => {
   toast("successful");
@@ -13,26 +14,37 @@ invalidBtn.addEventListener("click", () => {
   toast("invalid");
 })
 
+infoBtn.addEventListener("click", () => {
+  toast("info");
+})
+
 let successfulMsg = "Successfully submitted!"
 let errorMsg = "Error occured, Please check agin!"
 let invalidMsg = "Invalid data!"
+let infoMsg = "This is an info toast!"
 
 function toast(message) {
   const toast = document.createElement("div");
   toast.classList.add(`${message}-toast`);
   toast.classList.add(`toast`);
+
   if (message === "successful") {
     toast.innerHTML = `<img src="./images/${message}.png">
   ${successfulMsg}`;
   } else if (message === "error") {
     toast.innerHTML = `<img src="./images/${message}.png">
     ${errorMsg}`;
-  } else {
+  } else if (message === "info") {
+    toast.innerHTML = `<img src="./images/${message}.png">
+    ${infoMsg}`;
+  }
+  else {
     toast.innerHTML = `<img src="./images/${message}.png">
     ${invalidMsg}`;
   }
 
   toastBox.appendChild(toast);
+
   setTimeout(() => {
     toast.remove();
   }, 3000);
